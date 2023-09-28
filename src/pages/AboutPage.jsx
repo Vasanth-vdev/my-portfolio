c53fb830f9ab6c5098eca8c798db5d6121b15b90
Vasanth-vdev <vasanthg1240@gmail.com>
2023-09-28 18:22:28 +0530

    added sliding animation

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Cursor} from 'react-simple-typewriter'
 import photo from '../assets/photo.jpg'
 import {useTypewriter} from 'react-simple-typewriter'
 
 const AboutPage = () => {
+  const [activeTab,setActiveTab] = useState('about')
     const [text] = useTypewriter({
         words : ['VASANTH','FULL STACK WEB DEVELOPER'],
         loop : {},
       })
+      const handleTab = (tabName) => {
+        setActiveTab(tabName)
+      }
   return (
     <div>
       <div className='header'>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Projects</li>
-          <li>Contact</li>
+          <li className={activeTab === 'about' ? 'active' : ''}
+          onClick={() => handleTab('about')}>About</li>
+          <li className={activeTab === 'skills' ? 'active' : ''}
+          onClick={() => handleTab('skills')}>Skills</li>
+          <li className={activeTab === 'projects' ? 'active' : ''}
+          onClick={() => handleTab('projects')}>Projects</li>
+          <li className={activeTab === 'contact' ? 'active' : ''}
+          onClick={() => handleTab('contact')}>Contact</li>
       </div>
       <div className='description'>
         <div className='myname'>
@@ -23,7 +31,8 @@ const AboutPage = () => {
           <span style={{color:'blue'}}><Cursor cursorStyle='⚡'/></span></p>
         </div>
         <div className='imageContainer'>
-          <img loading='lazy' width={500} height={500} src={photo} />
+          <img loading='lazy'
+          className='show' width={500} height={500} src={photo} />
         </div>
       </div>
     </div>
